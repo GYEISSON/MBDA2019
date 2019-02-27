ebc908be9fac735986b47df7c5f1d967d401e3e7
srubianof <43104565+srubianof@users.noreply.github.com>
2019-02-27 18:50:19 -0500

    Avance
    poblando tablas

--- auto03/tablas.sql
+++ auto03/tablas.sql
@@ -2,18 +2,30 @@
 CREATE TABLE Rate(
   room_type VARCHAR2(6) NOT NULL,
   occupancy NUMBER(11) NOT NULL,
-  amount NUMBER(10,4)
+  amount NUMBER(100)
 );
+
+ALTER TABLE Rate 
+ADD CONSTRAINT ROOMTYP PRIMARY KEY(room_type) ;
+
+ALTER TABLE Rate 
+ADD CONSTRAINT OCCUP PRIMARY KEY(occupancy) ;
+
 
 CREATE TABLE Room_type(
     id VARCHAR(6) NOT NULL,
-    description VARCHAR2(100)
+    description VARCHAR(1000)
 );
+ALTER TABLE Room_type 
+ADD CONSTRAINT ROMTYPID PRIMARY KEY(id) ;
 
 CREATE TABLE Room(
     id VARCHAR(6) NOT NULL,
+    room_type VARCHAR2(6),
     max_occupancy NUMBER(11)
 );
+ALTER TABLE Room ADD CONSTRAINT ROOMID PRIMARY KEY(id) ;
+ALTER TABLE Room ADD CONSTRAINT FK_ROOM_TYPE FOREIGN KEY (room_type) REFERENCES Room_type(id);
 
 CREATE TABLE Guest(
     id VARCHAR2(6) NOT NULL,
@@ -21,6 +33,7 @@ CREATE TABLE Guest(
     last_name VARCHAR2(32) NOT NULL,
     address VARCHAR(32) NOT NULL
 );
+ALTER TABLE Guest ADD CONSTRAINT GUESTID PRIMARY KEY(id) ;
 
 CREATE TABLE Booking (
     booking_id VARCHAR(6) NOT NULL,
@@ -32,13 +45,24 @@ CREATE TABLE Booking (
     nights NUMBER(6) NOT NULL,
     arrival_time DATE NOT NULL
 );
+ALTER TABLE Booking ADD CONSTRAINT BOOKID PRIMARY KEY(booking_id);
+ALTER TABLE Booking ADD CONSTRAINT FK_ROOM_NO FOREIGN KEY(room_no) REFERENCES Room(id);
+ALTER TABLE Booking ADD CONSTRAINT FK_GST_ID FOREIGN KEY(guest_id) REFERENCES Guest(id);
+ALTER TABLE Booking ADD CONSTRAINT FK_OCCUPNTS FOREIGN KEY(occupants) REFERENCES Rate(occupancy);
+ALTER TABLE Booking ADD CONSTRAINT FK_ROOM_TYRQSTD FOREIGN KEY(room_type_requested) REFERENCES Room_type(id);
+
 
 CREATE TABLE Extra(
     extra_id VARCHAR2(6) NOT NULL,
     booking_id vARCHAR(6) NOT NULL,
-    desscription VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
     amount NUMBER(10,4) NOT NULL
 
 );
 
-select * from 
+
+--pob
+
+
+
+
