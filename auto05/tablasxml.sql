a464b31a65d487038fcc6216990b0054cb271b85
srubianof <43104565+srubianof@users.noreply.github.com>
2019-04-10 18:21:52 -0500

    Update tablasxml.sql

--- auto05/tablasxml.sql
+++ auto05/tablasxml.sql
@@ -13,9 +13,9 @@ INSERT INTO ROOM VALUES (101,'SINGLE',1,
 '<?xml version="1.0"?>
  <Room>
     <Bed 
-        dobles=1
-        sencillas=0 
-        camarotes=0>      
+        dobles="1"
+        sencillas="0"
+        camarotes="0">      
     </Bed>
     <Floor 
         Baldosas = "NO" 
@@ -52,5 +52,97 @@ INSERT INTO ROOM VALUES (101,'SINGLE',1,
         <Comment 
             fecha = "09/04/2012" 
             estrellas ="2">justificacion</Comment>
+    </Comments>
+</Room>');
+
+INSERT INTO ROOM VALUES (102,'DOUBLE',2,
+'<?xml version="1.0"?>
+ <Room>
+    <Bed 
+        dobles="2"
+        sencillas="0"
+        camarotes="0">      
+    </Bed>
+    <Floor 
+        Baldosas = "NO" 
+        Tapete = "NO" 
+        Madera="SI"> 
+    </Floor>
+    
+    <Showers >
+    </Showers>
+
+    <Views>
+        <interior></interior>
+        <jardin></jardin>
+        <montana></montana>
+        <piscina></piscina>
+        <mar></mar>
+    </Views>
+
+    <Decorations>
+        <Decoration 
+            nombre = "HOLA2" 
+            clase = "2"
+            autor="BBB">
+        </Decoration>
+    </Decorations>
+    
+    <Colors></Colors>
+    
+    <Canals 
+        satelite ="NO" 
+        cable ="SI">
+    </Canals>
+    <Comments>
+        <Comment 
+            fecha = "02/07/2009" 
+            estrellas ="1">justificacion</Comment>
+    </Comments>
+</Room>');
+
+INSERT INTO ROOM VALUES (103,'FAMILY',3,
+'<?xml version="1.0"?>
+ <Room>
+    <Bed 
+        dobles="3"
+        sencillas="0"
+        camarotes="0">      
+    </Bed>
+    <Floor 
+        Baldosas = "SI" 
+        Tapete = "NO" 
+        Madera="NO"> 
+    </Floor>
+    
+    <Showers >
+    </Showers>
+
+    <Views>
+        <interior></interior>
+        <jardin></jardin>
+        <montana></montana>
+        <piscina></piscina>
+        <mar></mar>
+    </Views>
+
+    <Decorations>
+        <Decoration 
+            nombre = "ABCD" 
+            clase = "4"
+            autor="CCCC">
+        </Decoration>
+    </Decorations>
+    
+    <Colors></Colors>
+    
+    <Canals 
+        satelite ="SI" 
+        cable ="NO">
+    </Canals>
+    <Comments>
+        <Comment 
+            fecha = "11/08/2019" 
+            estrellas ="5">justificacion</Comment>
     </Comments>
 </Room>');
