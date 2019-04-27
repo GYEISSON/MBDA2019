6d05a962385eae8cba8f26f50162e336787751e5
GYEISSON <yeison7f@gmail.com>
2019-04-27 10:33:34 -0500

    consulta

--- lab05/Instaknow/Instaknow.sql
+++ lab05/Instaknow/Instaknow.sql
@@ -763,7 +763,9 @@ DROP  TABLE Perfil;
 --Consultas 
 
 
-select *
+select contenidoTema.tema as Tema, temporal.url as URL,count(opiniones.numero) as Opiniones
 from contenidoTema,Temporal,Opinion
-where contenidoTema.contenido = temporal.nombre and temporal.nombre = opinion.contenido ;      
+where contenidoTema.contenido = temporal.nombre and temporal.nombre = opinion.contenido
+and detalle = 'momentos de error'
+Group by opiniones.numero;
     
