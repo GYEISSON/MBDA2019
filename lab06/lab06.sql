d91acc3feab532da87ead8c6ca227044597a0120
unknown <2127790@labinfo.is.escuelaing.edu.co>
2019-05-03 07:58:08 -0500

    perimeros avances

---
+++ lab06/lab06.sql
@@ -0,0 +1,9 @@
+--B 
+INSERT INTO CONTENIDOS VALUES('yeison7f@gmail.com','yeisson','n','computer science','www.goojle.com','V',12,'E','software');
+INSERT INTO CONTENIDOS VALUES('yeison7f@gmail.com','YEISSON','N','computer science','www.goojle.com','F',12,'E','software');
+
+--C
+UPDATE  CONTENIDOS SET NOMBRE ='fabian' WHERE correo = 'yeison7f@gmail.com';
+DELETE FROM CONTENIDOS WHERE correo = 'yeison7f@gmail.com';
+
+ 
