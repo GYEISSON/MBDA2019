c0eec724ce318de3e641199abf9825854244ebeb
GYEISSON <yeison7f@gmail.com>
2019-05-07 11:13:38 -0500

    procedimiento actualizar en paquete opinion

--- lab06/CRUDI.sql
+++ lab06/CRUDI.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PACKAGE BODY PC_OPINIONES IS 
-
+  
   PROCEDURE AD_OPINION (xtipo IN VARCHAR, xjustificacion IN VARCHAR, xdetalle IN VARCHAR, xperfilc IN VARCHAR, xcontenidoid IN VARCHAR) IS
     BEGIN
       INSERT INTO OPINION (tipo ,justificacion ,detalle ,perfilc ,contenidoid) VALUES (xtipo ,xjustificacion ,xdetalle ,xperfilc ,xcontenidoid);
@@ -9,15 +9,25 @@ CREATE OR REPLACE PACKAGE BODY PC_OPINIONES IS
         ROLLBACK;
         RAISE_APPLICATION_ERROR(-20010, 'No se puede insertar opinion.');
     END;
-    
+
+  PROCEDURE MOD_OPINION (XNUMERO IN NUMBER, XDETALLE IN XMLTYPE) IS
+  BEGIN
+    UPDATE OPINION SET DETALLE = XDETALLE WHERE NUMERO=XNUMERO; 
+    COMMIT;
+    EXCEPTION
+    WHEN OTHERS THEN 
+      ROLLBACK;
+      RAISE_APPLICATION_ERROR(-20019,"No se puede actualizar la opinion");
+  END;
   PROCEDURE EL_OPINION (xnumero IN NUMBER) IS 
-    BEGIN 
-      DELETE FROM OPINION WHERE  numero  = xnumero;
-	COMMIT; 
-	EXCEPTION
-      WHEN OTHERS THEN 
-       ROLLBACK;
-       RAISE_APPLICATION_ERROR(-20000,'Error al eliminar opinion.');
+  
+  BEGIN 
+    DELETE ON OPINION WHERE  numero  = xnumero;
+  	COMMIT; 
+  	EXCEPTION
+        WHEN OTHERS THEN 
+         ROLLBACK;
+         RAISE_APPLICATION_ERROR(-20000,'Error al eliminar opinion.');
    END;
 END PC_OPINIONES;
 
